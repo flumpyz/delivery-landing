f7d5e4377cd049a781e1175a7e4ca33b12a03a63
flumpyz <natalianata2001@mail.ru>
2022-02-01 17:21:41 +0500

    made up main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo_sign from './images/logo-sign.svg';
+import logo_text from './images/logo-text.svg';
+import phone_icon from './images/phone.svg';
 import './App.css';
+import './reset.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <main className="home-page">
+                <header className="header">
+                    <div className="logo">
+                        <img className="logo__logo-sign" src={logo_sign} alt="" />
+                        <img className="logo__logo-text" src={logo_text} alt="" />
+                    </div>
+                    <div className="vertical-line">
+                    </div>
+                    <div className="company-name">
+                        <p className="company-name__title">Курьерская служба доставки</p>
+                    </div>
+                    <div className="contact-number">
+                        <img className="contact-number__icon" src={phone_icon} alt=""/>
+                        <p className="contact-number__number">8 (909) 023-12-51</p>
+                    </div>
+                    <div className="login">
+                        <button className="login__button">
+                            Вход
+                        </button>
+                    </div>
+                </header>
+                <div className="slider">slider</div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
